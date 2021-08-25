fd61775f84e2bc6d62f64168102b0fb1e192f480
Marinho Kraemer <marinhokraemer97@gmail.com>
2021-08-25 20:19:32 -0300

    Typescript

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {
     TouchableOpacity, 
+    TouchableOpacityProps,
     Text,
     StyleSheet
 } from 'react-native';
 
-export function SkillCard({ skill }){
+interface SkillCardProps extends TouchableOpacityProps {
+    skill: string;
+};
+
+export function SkillCard({ skill, ...rest } : SkillCardProps ){
     return (        
         <TouchableOpacity 
             style={styles.buttonSkill}
             activeOpacity={.7}
+            {...rest}
             >
                 <Text style={styles.textSkill}>
                     {skill}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,102 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
+import {Button} from '../components/Button';
+import {SkillCard} from '../components/SkillCard';
+
+interface SkillData {
+  id: string;
+  name: string;
+};
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [gretting, setGretting] = useState('');
+
+  function handleAddNewSkill() {
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    };
+
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill (id: string) {
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ));
+  };
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGretting('Good Morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGretting('Good Afternoon');
+    } else if (currentHour >= 18) {
+      setGretting('Good Evening');
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, Marinho Kraemer</Text>
+
+      <Text style={styles.grettings}>{gretting}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+
+      <Button title="Add" onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => 
+          <SkillCard 
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  grettings: {
+    color: '#FFF',
+  },
+});
